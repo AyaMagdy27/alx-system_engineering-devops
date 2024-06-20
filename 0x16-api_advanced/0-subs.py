393109d933db9085991848df73a53a4acb1cc344
AyaMagdy27 <ayamado1996@gmail.com>
2024-06-20 15:56:06 -0700

    tes156

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,18 @@
-# 0-subs.py
+#!/usr/bin/python3
+""" script to obtain subscribers
+    count from a subreddit
+"""
+from requests import get
 
-import requests
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-    }
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        print("Response Status Code: {}".format(response.status_code))  # Debug print
-        if response.status_code == 200:
-            data = response.json()
-            print("Response JSON Data: {}".format(data))  # Debug print
-            if 'data' in data and 'subscribers' in data['data']:
-                return data['data']['subscribers']
-        elif response.status_code == 404:
-            print("Subreddit not found (404).")  # Debug print
-            return 0
-        else:
-            print("Unexpected status code: {}".format(response.status_code))  # Debug print
-    except requests.RequestException as e:
-        print("An error occurred: {}".format(e))
-        return 0
-    return 0
-
+    """ function to get subscriber count"""
+    if subreddit and type(subreddit) is str:
+        subscribers = 0
+        url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
+        headers = {'user-agent': 'my-app/0.0.1'}
+        req = get(url, headers=headers)
+        if req.status_code == 200:
+            data = req.json()
+            subscribers = data.get('data', {}).get('subscribers', 0)
+        return subscribers
