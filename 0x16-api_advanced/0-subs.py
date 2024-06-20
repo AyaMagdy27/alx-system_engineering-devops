1427b0aabddb3e786cb3f1a67ec939f2e3e4fd57
AyaMagdy27 <ayamado1996@gmail.com>
2024-06-20 16:07:22 -0700

    t

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     """ function to get subscriber count"""
     if subreddit and type(subreddit) is str:
         subscribers = 0
-        url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
+        url = 'https://www.reddit.com/dev/api//r/{}/about.json'.format(subreddit)
         headers = {'user-agent': 'my-app/0.0.1'}
         req = get(url, headers=headers)
         if req.status_code == 200:
